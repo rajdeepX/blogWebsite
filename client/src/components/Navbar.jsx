635c4b031ba80ebb13f808743457a70fd5d167b3
Rajdeep <rajdeepdutta70@gmail.com>
2023-05-22 23:21:25 +0530

    Login and logout authentication using jwt

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,14 +1,46 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [username, setUsername] = useState(null);
+
+  const fetchProfile = async () => {
+    const response = await fetch("http://localhost:3000/profile", {
+      credentials: "include",
+    });
+    const userInfo = await response.json();
+    setUsername(userInfo.username);
+  };
+
+  useEffect(() => {
+    fetchProfile();
+  }, []);
+
+  const logout = () => {
+    fetch("http://localhost:3000/logout", {
+      credentials: "include",
+      method: "POST",
+    });
+    setUsername(null);
+  };
+
   return (
     <header>
       <Link to={"/"} className="logo">
         MyBlog
       </Link>
       <nav>
-        <Link to={"/login"}>Login</Link>
-        <Link to={"/register"}>Register</Link>
+        {username ? (
+          <>
+            <Link to={"/create"}>Create New Post</Link>
+            <a onClick={logout}>Logout</a>
+          </>
+        ) : (
+          <>
+            <Link to={"/login"}>Login</Link>
+            <Link to={"/register"}>Register</Link>
+          </>
+        )}
       </nav>
     </header>
   );
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,13 +1,56 @@
+import { useState } from "react";
 import Navbar from "../components/Navbar";
+import { Navigate } from "react-router-dom";
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [redirect, setRedirect] = useState(false);
+
+  const options = {
+    method: "POST",
+    body: JSON.stringify({ username, password }),
+    headers: { "Content-Type": "application/json" },
+    credentials: "include",
+  };
+
+  const fetchApi = async () => {
+    const response = await fetch("http://localhost:3000/login", options);
+    if (response.ok) {
+      setRedirect(true);
+    } else {
+      alert("Wrong credentials!");
+    }
+  };
+
+  const login = (e) => {
+    e.preventDefault();
+    fetchApi();
+  };
+
+  if (redirect) {
+    return <Navigate to={"/"} />;
+  }
+
   return (
     <>
       <Navbar />
-      <form className="login" action="">
+      <form className="login" action="" onSubmit={login}>
         <h1>Login</h1>
-        <input type="text" name="username" placeholder="username" />
-        <input type="password" name="password" placeholder="password" />
+        <input
+          type="text"
+          name="username"
+          placeholder="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <button>Login</button>
       </form>
     </>
